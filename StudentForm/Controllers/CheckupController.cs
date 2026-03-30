a3257aec60fb9fcda455321ac2e9ef4ea8e3f0ae
LALTECH\abubakar_291 <muhammadabubakarkhan1234@gmail.com>
2026-03-30 10:29:11 +0500

    two db tables one model nd controller

--- StudentForm/Controllers/CheckupController.cs
+++ StudentForm/Controllers/CheckupController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Patient_Form.Data;
 using Patient_Form.Models;
+using System;
 using System.Linq;
 using CheckupModel = Patient_Form.Models.CheckupModel;
 
@@ -24,27 +27,53 @@ namespace Patient_Form.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult OnlineCheckup(CheckupModel model)
-        {
-            if (!ModelState.IsValid) {
-                //ViewBag.Appointments = _context.Set<CheckupModel>().ToList();
+            {
+            if (!ModelState.IsValid)
                 return View(model);
+
+            using (var conn = _context.Database.GetDbConnection()) {
+                conn.Open();
+
+                int patientId;
+                using (var cmd = conn.CreateCommand()) {
+                    cmd.CommandText = @"
+                INSERT INTO dbo.Patients 
+                (FName, LstName, Age, Gender, DOB, Email, Phone, City)
+                VALUES (@FName,@LstName,@Age,@Gender,@DOB,@Email,@Phone,@City);
+                SELECT CAST(SCOPE_IDENTITY() as int);";
+
+                    cmd.Parameters.Add(new SqlParameter("@FName", model.FName));
+                    cmd.Parameters.Add(new SqlParameter("@LstName", model.LstName));
+                    cmd.Parameters.Add(new SqlParameter("@Age", model.Age));
+                    cmd.Parameters.Add(new SqlParameter("@Gender", model.Gender));
+                    cmd.Parameters.Add(new SqlParameter("@DOB", (object)model.DOB ?? DBNull.Value));
+                    cmd.Parameters.Add(new SqlParameter("@Email", model.Email));
+                    cmd.Parameters.Add(new SqlParameter("@Phone", model.Phone));
+                    cmd.Parameters.Add(new SqlParameter("@City", model.City));
+
+                    patientId = (int)cmd.ExecuteScalar();
+                }
+
+                using (var cmd = conn.CreateCommand()) {
+                    cmd.CommandText = @"
+                INSERT INTO dbo.Appointments
+                (PatientId, PatientType, VisitType, Disease, AppointmentDate, SlotTime, Message)
+                VALUES (@PatientId,@PatientType,@VisitType,@Disease,@AppointmentDate,@SlotTime,@Message)";
+
+                    cmd.Parameters.Add(new SqlParameter("@PatientId", patientId));
+                    cmd.Parameters.Add(new SqlParameter("@PatientType", model.PatientType));
+                    cmd.Parameters.Add(new SqlParameter("@VisitType", model.VisitType));
+                    cmd.Parameters.Add(new SqlParameter("@Disease", model.Disease));
+                    cmd.Parameters.Add(new SqlParameter("@AppointmentDate", model.AppointmentDate.Value));
+                    cmd.Parameters.Add(new SqlParameter("@SlotTime", model.SlotTime));
+                    cmd.Parameters.Add(new SqlParameter("@Message", (object)model.Message ?? DBNull.Value));
+
+                    cmd.ExecuteNonQuery();
+                }
             }
-
-            bool slotTaken = _context.Set<CheckupModel>().Any(x =>
-                x.AppointmentDate.Value.Date == model.AppointmentDate.Value.Date &&
-                x.SlotTime == model.SlotTime);
-
-            if (slotTaken) {
-                ModelState.AddModelError("SlotTime", "This slot is already booked!");
-                ViewBag.Appointments = _context.Set<CheckupModel>().ToList();
-                return View(model);
-            }
-
-            _context.Set<CheckupModel>().Add(model);
-            _context.SaveChanges();
 
             TempData["Success"] = "Appointment booked successfully!";
-            return RedirectToAction("CheckupModel");
+            return RedirectToAction("OnlineCheckup");
         }
     }
 }
